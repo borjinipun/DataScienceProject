f44c959bac043f67551ac5a201de276c80c6b56f
borjinipun <borjinipun@gmail.com>
2023-04-09 14:59:29 +0530

    Project loggin and exception

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-from setuptools import find_namespace_packages,setup
+from setuptools import find_packages,setup
 from typing import List
 
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
-    # this function will return the list of get_requirements
+    '''
+    this function will return the list of requirements
+    '''
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
 
-
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 setup(
     name='DataScienceProject',
     version='0.0.1',
     author='Nipun',
     author_email='borjinipun@gmail.com',
-    package_data=find_namespace_packages(),
+    package_data=find_packages(),
     requires=get_requirements('requirements.txt')
 )
